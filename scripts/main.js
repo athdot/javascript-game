55bd82e7ef7361f5c6a2f77e4b7c8df76581cfb2
athdot <athdot@gmail.com>
2017-04-14 11:15:18 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
         var ifrm = document.createElement("iframe");
         ifrm.setAttribute("src", "https://athdot.github.io/javascript-game/levels/01/level.html");
         ifrm.style.width = "100%";
@@ -6,4 +29,19 @@
         ifrm.style.position = "absolute";
         ifrm.style.top = "0";
         ifrm.style.left = "0";
+        ifrm.id = "cookie";
         document.body.appendChild(ifrm);
+    var user=getCookie("level");
+    if (user != "") {
+    var level=user
+    var url = ('https://athdot.github.io/javascript-game/levels/' + level + '/level.html');
+document.getElementById('cookie').src = url;
+    } else {
+       user = "01"
+           var level=user
+               var url = ('https://athdot.github.io/javascript-game/levels/' + level + '/level.html');
+document.getElementById('cookie').src = url
+       if (user != "" && user != null) {
+           setCookie("level", user, 30);
+       }
+    }
