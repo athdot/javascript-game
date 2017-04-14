b7a7316125f0a8ca039900bb972845166439bd8c
athdot <athdot@gmail.com>
2017-04-14 10:46:24 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-alert("Hi")
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "levels/01/level.html");
+        ifrm.style.width = "100%";
+        ifrm.style.height = "100vh";
+        ifrm.style.border = "0";
+        document.body.appendChild(ifrm);
