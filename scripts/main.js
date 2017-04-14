f4d947a3873dc7a47ca9c0757b623ea11e6d01d2
athdot <athdot@gmail.com>
2017-04-14 10:53:15 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,8 @@
         ifrm.setAttribute("src", "https://athdot.github.io/javascript-game/levels/01/level.html");
         ifrm.style.width = "100%";
         ifrm.style.height = "100vh";
-ifrm.style.border = "0";
+        ifrm.style.border = "0";
+        ifrm.style.position = "absolute";
+        ifrm.style.top = "0";
+        ifrm.style.left = "0";
         document.body.appendChild(ifrm);
