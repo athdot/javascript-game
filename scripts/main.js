d07f20d767540735953ec57b11039c4f73089a4e
athdot <athdot@gmail.com>
2017-04-14 10:51:38 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
         var ifrm = document.createElement("iframe");
         ifrm.setAttribute("src", "https://athdot.github.io/javascript-game/levels/01/level.html");
-        ifrm.style.width = "100px";
-        ifrm.style.height = "100px";
+        ifrm.style.width = "100%";
+        ifrm.style.height = "100vh";
+ifrm.style.border = "0";
         document.body.appendChild(ifrm);
