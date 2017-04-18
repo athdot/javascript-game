3ff6db70ed996dbc58e50d2dd038ad46e16bcaf6
athdot <athdot@gmail.com>
2017-04-18 10:16:54 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -37,7 +37,7 @@ function getCookie(cname) {
     var url = ('https://athdot.github.io/javascript-game/levels/' + level + '/level.html');
 document.getElementById('cookie').src = url;
     } else {
-       user = "01"
+       user = "02"
            var level=user
                var url = ('https://athdot.github.io/javascript-game/levels/' + level + '/level.html');
 document.getElementById('cookie').src = url
